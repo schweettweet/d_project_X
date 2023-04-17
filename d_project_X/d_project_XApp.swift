2bab1423eaa54676b20f61d366bf1c267fd1e5b4
schweettweet <schweettweet@gmail.com>
2023-04-17 17:57:23 -0400

    Initial Commit

---
+++ d_project_X/d_project_XApp.swift
@@ -0,0 +1,17 @@
+//
+//  d_project_XApp.swift
+//  d_project_X
+//
+//  Created by Scott Wachtler on 4/17/23.
+//
+
+import SwiftUI
+
+@main
+struct d_project_XApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
